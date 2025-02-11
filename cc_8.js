6e7d5732034a6d8726610d85edcebf17b5d3a96f
Rachael <rachy.torok@gmail.com>
2025-02-11 16:19:15 -0500

    Task 2 - Product Price After Discount.

--- cc_8.js
+++ cc_8.js
@@ -6,3 +6,15 @@ function calculateSalary(baseSalary, bonus, taxRate) {
 
 console.log(calculateSalary(5000, 500, 0.1)); // Expected output: "Net Salary : $5000.00" 
 console.log(calculateSalary(7000, 1000, 0.15)); // Expected output: "Net Salary : $6950.00"
+
+
+// Task 2 : Product Price After Discount
+
+function calculateDiscount(price, discountRate) {
+    let finalPrice = price - (price * discountRate); // formula for final price
+    return `Final Price: $${finalPrice.toFixed(2)}`; // return the final price
+};
+
+console.log(calculateDiscount(100, 0.2)); // Expected output: "Final Price: $80.00"
+console.log(calculateDiscount(250, 0.15)); // Expected output: "Final Price: $212.50"
+
